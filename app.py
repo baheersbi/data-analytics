68cdd850c538d155e0eb6d6cb30bfc1ae6fce521
baheersbi <baseer.baheer@berlinsbi.com>
2024-04-18 15:45:34 +0200

    Calculator added

--- app.py
+++ app.py
@@ -1,9 +1,35 @@
-print ("Hello World!")
+# a = 90
+# print(a)
+a = input("Please enter the first number:")
+b = input("Please enter the second number:")
+print("Please select the operator:")
+# print(op[2])
+op = ["Addition", "Subtraction", "Multiplication", "Division"]
+i =1
+for x in op:
+    print(str(i) +". " + x)
+    i=i+1
 
-a = 34
-b = 89
-
-if a>b:
-    print("a is greater than b")
+op = input("Your choice:")
+if op=="1":
+    print("The sum of these two numbers:" + str(int(a) + int(b)))
+elif op=="2":
+    print("The subtraction of these two numbers:" + str(int(a) - int(b)))
+elif op=="3":
+    print("The multiplication of these two numbers:" + str(int(a) * int(b)))
 else:
-    print("b is greated than a")
+    print("The division of these two number:" + str(int(a) / int(b)))
+# for x in op:
+#     if x=="Multiplication":
+#         break
+#     print(x)
+
+# for x in op:
+#     if x=="Multiplication":
+#         continue
+#     print(x)
+
+# print(op[2])
+# print(len(op))
+
+
